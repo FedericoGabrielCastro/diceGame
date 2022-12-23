e8ea54c29e7e76148c30cb5588dda12e2346bbb1
federico <fgc_castro@live.com>
2022-12-23 13:52:30 -0300

    [DOTENV]
    Connect envs with the app

---
+++ obiwan-server/src/config/index.ts
@@ -0,0 +1,33 @@
+import doteenv from "dotenv"
+
+/** 
+ * Config doteenv.
+ * 
+ * Purpose:
+ * - Provides an instance for setting environment variables
+ * 
+ * References:
+ * - https://www.freecodecamp.org/news/how-to-use-node-environment-variables-with-a-dotenv-file-for-node-js-and-npm/
+ * - https://yarnpkg.com/package/@types/dotenv
+ */
+const envFound = doteenv.config()
+
+/** 
+ * Error conditional.
+ * 
+ * Purpose:
+ * - If it doesn't find the requested environment variable, it sends an error message.
+ */
+if(envFound.error) {
+    throw new Error("Couldn't find .env file")
+}
+
+/** 
+ * Export env.
+ * 
+ * Purpose:
+ * - Export enviroment variable.
+ */
+export default {
+    port: process.env.PORT
+}
